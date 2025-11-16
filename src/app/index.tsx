97d57b3144b22ec9ec4dbf5f39ee6bea6db08461
Pedro Silva <pedro_rike@hotmail.com.br>
2025-11-16 16:37:49 -0300

    feat: add component welcome

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Welcome } from "@/components/welcome";
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 22, fontWeight: "bold" }}>Hello, World!</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   );
 }
